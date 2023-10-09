c04eae9eee7560b9087bf13bad68526472e19cea
Alan Maginn <p.alan.maginn@gmail.com>
2023-10-09 07:30:54 -0400

    Write game results for one round

--- RPS.js
+++ RPS.js
@@ -4,3 +4,34 @@ const computerOptions = ["Rock", "Paper", "Scissors"]
 function getComputerChoice() {
     return computerOptions[~~(Math.random() * computerOptions.length)]
 }
+
+// Create a function that allows a player to play one round with the computer
+function playRound(playerSelection, computerSelection) {
+    // to lower case
+    playerSelection = playerSelection.toLowerCase();
+    // if player choice == rock
+    if (playerSelection == 'rock' && computerSelection == 'Paper')
+        return 'You Lose!';
+    else if (playerSelection == 'rock' && computerSelection == 'Rock')
+        return "Tie game!";
+    else if (playerSelection == 'rock' && computerSelection == 'Scissors')
+        return "Winner Winner!!"
+    // if player choice == paper
+    else if (playerSelection == 'paper' && computerSelection == 'Scissors')
+        return "You Lose!"
+    else if (playerSelection == 'paper' && computerSelection == 'Paper')
+        return "Tie Game!"
+    else if (playerSelection == 'paper' && computerSelection == 'Rock')
+        return "Winner Winner!!"
+    // if player choice == scissors
+    else if (playerSelection == 'scissors' && computerSelection == 'Rock')
+        return "You Lose!"
+    else if (playerSelection == 'scissors' && computerSelection == 'Scissors')
+        return "Tie Game!"
+    else if (playerSelection == 'scissors' && computerSelection == 'Paper')
+        return "Winner Winner!!"
+}
+// check code viability
+  const playerSelection = "rock";
+  let computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));
